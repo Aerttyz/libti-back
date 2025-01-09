d599e7c8f2c1ce1f62b31a1b2acd2f7b0357c951
Aerttyz <alesandrosilva2004@gmail.com>
2025-01-08 21:59:18 -0300

    start spring API libti

---
+++ libti/src/test/java/com/libti/libti/LibtiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.libti.libti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibtiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
